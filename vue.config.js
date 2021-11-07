5acdeb153ffe7dac523c49c2a499f3bb7ffd11a4
Enrique Marin <emarin@applab.mx>
2021-11-07 11:17:31 -0700

    Update VueConfig

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,5 @@ module.exports = {
     msTileColor: '#FFFFFF',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
   }
 }
